9d794bf71581b3488012a08c168b7b2e72ece1ed
= <=>
2017-10-15 08:46:05 +0100

    intial commit

---
+++ Week5/JustFrame.java
@@ -0,0 +1,16 @@
+/**
+ A simple class to show a single frame
+*/
+
+//import java classes for GUI components
+import javax.swing.*;
+
+public class JustFrame extends JFrame
+{  
+    public JustFrame() {
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	setSize(300,300);
+	setLocation(100,100);
+	setTitle("Just a JFrame");
+    }
+}
