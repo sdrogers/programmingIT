477064a20450149aa05f8ef98e9a983f46d363ba
= <=>
2017-10-15 15:28:54 +0100

    fixing weird indentation

--- Week5_1/JustFrame.java
+++ Week5_1/JustFrame.java
@@ -7,10 +7,10 @@ import javax.swing.*;
 
 public class JustFrame extends JFrame
 {  
-    public JustFrame() {
-	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	setSize(300,300);
-	setLocation(100,100);
-	setTitle("Just a JFrame");
-    }
+	public JustFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300,300);
+		setLocation(100,100);
+		setTitle("Just a JFrame");
+	}
 }
