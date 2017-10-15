ad190ff7bd89ecbc85a88e2aab31fb08a48b8da8
= <=>
2017-10-15 08:52:32 +0100

    added first set of code

---
+++ Week5_1/ShowJustFrame.java
@@ -0,0 +1,12 @@
+/**
+ A simple class to show a single frame
+*/
+
+public class ShowJustFrame
+{  
+	public static void main(String [] args)
+	{
+		JustFrame frame = new JustFrame();
+		frame.setVisible(true);
+	}
+}
